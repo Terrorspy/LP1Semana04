62622a1da55e760eeb842c85eb2d69b1c5fdbcb0
Terrorspy <diogocaetanosoares922@gmail.com>
2022-03-17 15:47:17 +0000

    Adicionei comentários XML às funções, invoca a função CounTo10 duas vezes e imprime números ate n

--- MyFirstMethod/Program.cs
+++ MyFirstMethod/Program.cs
@@ -2,8 +2,14 @@
 
 namespace MyFirstMethod
 {
+    /// <summary>
+    /// Este programa invoca a função CountTo10 duas vezes e imprime os números até n que o utilizador pode alterar na invocação da função CountToN dentro da função CountTo10
+    /// </summary>
     class Program
     {
+        /// <summary>
+        /// Invoca a função CountTo10 duas vezes e recebe inputs do utilizador
+        /// </summary>
         private static void Main(string[] args)
         {
             string aux = Console.ReadLine();
@@ -13,10 +19,17 @@ namespace MyFirstMethod
             for(int num = 0; num < 2; num++)
                 CountTo10();
         }
+        
+        /// <summary>
+        /// Invoca a função CountToN e conta até n.
+        /// </summary>
         private static void CountTo10()
         {
             CountToN(10);
         }
+        /// <summary>
+        /// Cria um ciclo for e soma até chegar a n.
+        /// </summary>
 
         private static void CountToN(int n)
         {
