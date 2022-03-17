4ceb8c788064ac33073dcc07002846c5703673ae
Terrorspy <diogocaetanosoares922@gmail.com>
2022-03-17 15:30:43 +0000

    Alterei CountTo10 de modo invocar CountToN

--- MyFirstMethod/Program.cs
+++ MyFirstMethod/Program.cs
@@ -4,17 +4,25 @@ namespace MyFirstMethod
 {
     class Program
     {
-        static void Main(string[] args)
+        private static void Main(string[] args)
         {
+            string aux = Console.ReadLine();
+            int nCount = Convert.ToInt32( aux );
+            CountToN(nCount);
+
             for(int num = 0; num < 2; num++)
                 CountTo10();
         }
-
         private static void CountTo10()
         {
-            for ( int i = 1; i <= 10; i++)
+            CountToN(10);
+        }
+
+        private static void CountToN(int n)
+        {
+            for( int i = 1; i <= n; i++)
             {
-                Console.WriteLine( i );
+                Console.WriteLine(i);
             }
         }
     }
